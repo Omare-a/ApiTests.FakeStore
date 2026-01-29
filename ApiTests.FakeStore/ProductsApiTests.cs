295b031445f9425a4c6977a140246dca60cc5140
Ömer <omareany99@hotmail.com>
2026-01-29 21:11:21 +0100

    Add project files.

---
+++ ApiTests.FakeStore/ProductsApiTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiTests.FakeStore;
+
+public class ProductsApiTests
+{
+    private static readonly HttpClient _http = new()
+    {
+        BaseAddress = new Uri("https://fakestoreapi.com/"),
+        Timeout = TimeSpan.FromSeconds(15)
+    };
+
+    [Fact]
+    public async Task Get_Products_Returns_200_OK()
+    {
+        // Act
+        using var response = await _http.GetAsync("products");
+
+        // Assert
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+}
